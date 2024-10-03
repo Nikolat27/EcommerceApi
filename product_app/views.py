f26608e585a6b51528c820d06ccd468aa6808440
Nikolat27 <samalizadeh899@gmail.com>
2024-10-03 03:13:17 -0700

    I added related products

--- product_app/views.py
+++ product_app/views.py
@@ -2,14 +2,26 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+    authentication_classes,
+)
+import time
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 from . import serializers
 from rest_framework.response import Response
-from product_app.models import Product, Review, Comment, ProductLike, ReviewAction, CommentAction
+from product_app.models import (
+    Product,
+    Review,
+    Comment,
+    ProductLike,
+    ReviewAction,
+    CommentAction,
+)
 from .permissions import IsAuthenticatedOrReadOnly
 
 
@@ -29,25 +41,46 @@ class ProductViewSet(ViewSet, PageNumberPagination):
 
     def retrieve(self, request, pk=None):
         product = get_object_or_404(Product, slug=pk)
-        serializer = serializers.ProductSerializer(instance=product, many=False)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        product_serializer = serializers.ProductSerializer(instance=product, many=False)
+        related_products = Product.objects.filter(category__in=product.category.all())
+        rp = {}
+        for product in related_products:
+            rp["title"] = product.title,
+            rp["slug"] = product.slug,
+            rp["thumbnail"] = product.thumbnail.path
+            rp["discounted_price"] = product.discounted_price(),
+        
+        return Response(
+            {
+                "product": product_serializer.data,
+                "related_products": rp,
+            },
+            status=status.HTTP_200_OK,
+        )
 
     def create(self, request):
         serializer = serializers.ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response({"data": "Product Created successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(
+            {"data": "Product Created successfully!"}, status=status.HTTP_201_CREATED
+        )
 
     def update(self, request, pk=None):
         instance = get_object_or_404(Product, id=pk)
         serializer = serializers.ProductSerializer(data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.update(instance=instance, validated_data=serializer.validated_data)
-        return Response({"response": "Your Course has been updated successfully"}, status=status.HTTP_200_OK)
+        return Response(
+            {"response": "Your Course has been updated successfully"},
+            status=status.HTTP_200_OK,
+        )
 
     def delete(self, request, pk=None):
         Product.objects.get(id=pk).delete()
-        return Response({"data": "Product Deleted successfully!"}, status=status.HTTP_200_OK)
+        return Response(
+            {"data": "Product Deleted successfully!"}, status=status.HTTP_200_OK
+        )
 
 
 class ReviewProductView(APIView):
@@ -64,7 +97,9 @@ class ReviewProductView(APIView):
         author = request.user
         rating = request.get("rating")
         Review.objects.create(author=author, product_id=pk, text=text, rating=rating)
-        return Response({"data": "Review added successfully"}, status=status.HTTP_201_CREATED)
+        return Response(
+            {"data": "Review added successfully"}, status=status.HTTP_201_CREATED
+        )
 
 
 class CommentProductView(APIView):
@@ -84,11 +119,15 @@ class CommentProductView(APIView):
         if not parent_id:
             Comment.objects.create(author=author, product_id=pk, text=text)
         else:
-            Comment.objects.create(author=author, product_id=pk, text=text, parent_id=parent_id)
-        return JsonResponse({"data": "Comment Created successfully"}, status=status.HTTP_201_CREATED)
+            Comment.objects.create(
+                author=author, product_id=pk, text=text, parent_id=parent_id
+            )
+        return JsonResponse(
+            {"data": "Comment Created successfully"}, status=status.HTTP_201_CREATED
+        )
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def product_like(request, pk):
     try:
@@ -100,28 +139,40 @@ def product_like(request, pk):
         return Response({"data": "Product Liked!"}, status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def review_action(request, pk, action):
     if action not in ["like", "dislike"]:
-        return Response({"response": "Invalid action! your action_type must be either 'like' or 'dislike'"})
-    review, created = ReviewAction.objects.get_or_create(review_id=pk, user=request.user,
-                                                         defaults={"action_type": action})
+        return Response(
+            {
+                "response": "Invalid action! your action_type must be either 'like' or 'dislike'"
+            }
+        )
+    review, created = ReviewAction.objects.get_or_create(
+        review_id=pk, user=request.user, defaults={"action_type": action}
+    )
     if not created:
         review.action_type = action
         review.save()
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def comment_action(request, pk, action):
     if action not in ["like", "dislike"]:
-         return Response({"response": "Invalid action! your action_type must be either 'like' or 'dislike'"},
-                        status=status.HTTP_400_BAD_REQUEST)
-    comment, created = CommentAction.objects.get_or_create(comment_id=pk, user=request.user,
-                                                           defaults={"action_type": action})
+        return Response(
+            {
+                "response": "Invalid action! your action_type must be either 'like' or 'dislike'"
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    comment, created = CommentAction.objects.get_or_create(
+        comment_id=pk, user=request.user, defaults={"action_type": action}
+    )
     if not created:
         comment.action_type = action
         comment.save()
 
-    return Response({"response": "your action submitted successfully!"}, status=status.HTTP_200_OK)
+    return Response(
+        {"response": "your action submitted successfully!"}, status=status.HTTP_200_OK
+    )
