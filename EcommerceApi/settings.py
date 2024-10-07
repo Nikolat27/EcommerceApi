542ca0ce09df2e3b80dd802f56f5ce3fa3a9f2ae
Nikolat27 <samalizadeh899@gmail.com>
2024-10-08 00:14:03 +0330

    I added password reseting with email but idk why environment variables dont work !

--- EcommerceApi/settings.py
+++ EcommerceApi/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os.path
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -154,3 +156,17 @@ MEDIA_URL = os.path.join(BASE_DIR, "media/")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "user_auth_app.User"
+
+load_dotenv()
+
+print("Current Directory:", os.getcwd())
+print("Does .env exist?", os.path.isfile('.env'))
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
--- EcommerceApi/urls.py
+++ EcommerceApi/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from product_app.views import ProductViewSet, CategoryViewSet, BrandViewSet
 from . import settings
 
@@ -32,4 +33,8 @@ urlpatterns = [
                   path('user/', include("user_auth_app.urls")),
                   path('product/', include("product_app.urls")),
                   path('cart/', include("cart_app.urls")),
+                  path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+                  path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+                  path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+                  path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
